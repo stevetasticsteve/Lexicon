3a4bce2bfdefdc74bdc7612695b55bda0b74a6d7
steve <stevetasticsteve@gmail.com>
2020-05-13 19:53:07 +1000

    Switched from class based solution to plain functions

---
+++ lexicon_config.py
@@ -0,0 +1,26 @@
+settings = {
+            'language': 'Kovol',
+            'spreadsheet_name': '/media/NAS/Team Share/Kovol_lexicon.ods',
+            'target_folder': '/media/NAS/CLAHub/other_sites/Lexicon',                 # the folder the web page should be created in
+            'sort': 'phonetics',                             # order dictionary by 'phonetics' or 'orthography'
+        }
+
+spreadsheet_config = {
+            'id_col': 'A',            # Column containing id number
+            'orth_col': 'B',          # Column containing orthographic text
+            'phon_col': 'C',          # Column containing phonetics
+            'dial_col': 'D',          # Column containing dialect varient phonetics
+            'sense_col': 'E',         # Column containing sense number
+            'pos_col': 'F',           # Column containing part of speech
+            'eng_col': 'G',           # Column containing English translation
+            'tpi_col': 'H',           # Column containing Tok Pisin translation
+            'def_col': 'I',           # Column containing a definition
+            'ex_col': 'J',            # Column containing example(s)
+            'trans_col': 'K',         # Column containing example translation
+            'date_col': 'L',          # Column containing entry date
+            'enter_col': 'M',         # Column containing team member who entered data
+            'check_col': 'N',         # Column containing team member who checked
+            'syn_col': 'O',           # Column containing synonyms
+            'ant_col': 'P',           # Column containing antonyms
+            'link_col': 'Q',          # Column containing links,
+}
