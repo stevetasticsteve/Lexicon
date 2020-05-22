1d68d8d0f6426ec2b4406cabc4c0b5c02619ebaa
steve <stevetasticsteve@gmail.com>
2020-05-22 16:52:47 +1000

    Split things into different functions. Got it to display first character of each word

--- lexicon.py
+++ lexicon.py
@@ -15,6 +15,7 @@ def initiate_logging():
     # If working on Steve's laptop change source and target for dev work
     if socket.gethostname() == 'steve-stanley-latitude':
         s.settings['spreadsheet_name'] = 'Kovol_lexicon.ods'
+        # s.settings['spreadsheet_name'] = 'excel_test.xlsx'
         s.settings['target_folder'] = ''
         ch = logging.StreamHandler()
         formatter = logging.Formatter('%(message)s')
@@ -76,10 +77,6 @@ def read_lexicon():
         if entry['sense'] == '':
             entry['sense'] = 1
 
-    if s.settings['sort'] == 'orthography':
-        sort_orthographically(processed_data)
-    else:
-        sort_phonetically(processed_data)
     logger.info('%d dictionary entries read' % len(processed_data))
     return processed_data
 
@@ -94,6 +91,7 @@ def sort_phonetically(processed_data):
 
 def sort_orthographically(processed_data):
     return sorted(processed_data, key=lambda data: data['orth'])
+
 
 def sort_by_sense(processed_data):
     return sorted(processed_data, key=lambda data: data['sense'])
@@ -115,39 +113,22 @@ def HTML_header(title):
 
 
 def generate_help_page():
-    html_header = HTML_header('%s Lexicon' % s.settings['language'])
+    html_header = HTML_header('%s Lexicon help' % s.settings['language'])
     body = '<h1>HELP PAGE</h1>'
     html_close = '</div></body></html>'
     with open(os.path.join(s.settings['target_folder'], 'Lexicon_help.html'), 'w') as file:
         print(html_header, body, html_close, file=file)
 
 
-def generate_HTML():
-    data = read_lexicon()
-    # order by id number so the for loop sees all the senses of a word one after another
-    data = sort_by_sense(data) # get the sense numbers in order
-    data = sort_by_id(data)
-
-    # Create the HTML header and navbar
-    date = datetime.datetime.now().strftime('%A %d %B %Y')
-    html_header = HTML_header('%s Lexicon' % s.settings['language'])
-
-    btn_group = '''<div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-light">{0} - English</button>
-                    <button type="button" class="btn btn-dark">English - {0}</button>
-                    </div>'''.format(s.settings['language'])
-    body = '''
-    <body>
-    <div class="container-fluid p-3 mb-2 bg-info text-white">
-    <h1>%s Lexicon</h1> <div class="container-fluid float-right">%s 
-    <a href="Lexicon_help.html" button type="button" class="btn btn-light">Help</a> </div>
-    <p>Updated %s </p>
-    </div>
-    <div class="container-fluid" id="entries_pane">''' % (s.settings['language'], btn_group, date)
-
+def create_headwords_HTML(processed_data):
+    '''Takes the data and creates actual dictionary entries that takes account of multiple senses for the same word.
+    Returns a tuple (headword, html for entry) sorted alphabetically by headword'''
+    processed_data = sort_by_sense(processed_data) # get the sense numbers in order
+    processed_data = sort_by_id(processed_data)
+    lexicon_entries = []
     # Loop throgh the entries and create the dictionary entries
     last_id = 0
-    for entry in data:
+    for entry in processed_data:
         # choose phonetics for headword if orthography not available
         if entry['orth']:
             headword = entry['orth']
@@ -160,18 +141,26 @@ def generate_HTML():
             sense = '1.'
 
         lex_heading = '''
-        <div class="lexeme">
-        <h3>%s</h3>''' % headword
+            <div class="lexeme">
+            <h3>%s</h3>''' % headword
         # pos [phonetics] english, tpi : definition
         lex1 = '''
-        <p>%s <strong>%s</strong> [%s]
-        <strong><em style="color:dodgerblue"> %s</em></strong>, <strong style="color:gray">%s</strong> : %s</p>
-        ''' % (sense, entry['pos'], entry['phon'], entry['eng'], entry['tpi'], entry['def'])
-        examples = '''
-        <p>%s</p>
-        <p>%s</p>
-        ''' % (entry['ex'], entry['trans'])
-        lex_body = lex1 + examples
+            <p>%s <strong>%s</strong> [%s]
+            <strong><em style="color:dodgerblue"> %s</em></strong>, <strong style="color:gray">%s</strong>''' \
+               % (sense, entry['pos'], entry['phon'], entry['eng'], entry['tpi'])
+        if entry['def']:
+            definition = ': %s</p>' % entry['def']
+        else:
+            definition = '</p>'
+        if entry['ex']:
+            examples = '''<dl class="row">
+                <dt class="col-sm-1">%s</dt>
+                <dd class="col-sm-1">%s</dd>
+                </dl>
+                ''' % (entry['ex'], entry['trans'])
+        else:
+            examples = ''
+        lex_body = lex1 + definition + examples
 
         # If the entry is a sense of the previous iteration add the sense without a repeated header
         if entry['id'] == last_id:
@@ -180,18 +169,60 @@ def generate_HTML():
             lex_entry = lex_heading + lex_body
         last_id = entry['id']  # update previous id for this check
 
-        body += lex_entry  # add the entry to the HTML body
+        # create a tuple of the entry so it can be sorted by headword
+        lexicon_entries.append((headword, lex_entry))
+    lexicon_entries.sort()
+    return lexicon_entries
 
+
+def get_word_beginings(lexicon_entries):
+    '''Takes the tuple (headwords, entry html) and returns an alphabetically sorted set of the first letters of all
+     headwords'''
+    letters = [x[0][0] for x in lexicon_entries]
+    return sorted(set(letters))
+
+def generate_HTML():
+    # Create the HTML header and navbar
+    date = datetime.datetime.now().strftime('%A %d %B %Y')
+    html_header = HTML_header('%s Lexicon' % s.settings['language'])
+
+    btn_group = '''<div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-light">{0} - English</button>
+                    <button type="button" class="btn btn-dark">English - {0}</button>
+                    </div>'''.format(s.settings['language'])
+    navbar = '''
+    <body>
+    <div class="container-fluid p-3 mb-2 bg-info text-white">
+    <h1>%s Lexicon</h1> <div class="container-fluid float-right">%s 
+    <a href="Lexicon_help.html" button type="button" class="btn btn-light">Help</a> </div>
+    <p>Updated %s </p>
+    </div>''' % (s.settings['language'], btn_group, date)
+
+    data = read_lexicon()
+    lexicon_entries = create_headwords_HTML(data)
+    initial_letters = get_word_beginings(lexicon_entries)
+
+    main_div = '''<div class="container-fluid" id="main_body">
+    <div class="row">'''
+    side_bar = '''<div class=col-sm-1>''' % initial_letters
+    for letter in initial_letters:
+        side_bar += '''<ul>%s</ul>''' % letter
+    side_bar += '</div>'
+    entries_pane ='''<div class=col-sm-11>'''
+
+    # build the body of lexicon entries
+    for entry in lexicon_entries:
+        entries_pane += entry[1]
+    entries_pane += '</div>'
     # HTML closing tags
     html_close = '</div></body></html>'
 
+    body = main_div + side_bar + entries_pane + '</div>'
+
     # Write the document by joining header, body and close
     with open(os.path.join(s.settings['target_folder'], '%s_Lexicon.html') % s.settings['language'], 'w') as file:
-        print(html_header, body, html_close, file=file)
+        print(html_header, navbar, body, html_close, file=file)
     generate_help_page()
-
-
-
 
 
 if __name__ == '__main__':
--- sync_script.sh
+++ sync_script.sh
@@ -1,3 +1,5 @@
-#!usr/bin/bash
+#!/bin/bash
 
-rsync -v lexicon.py pi@pi:/media/NAS/CLAHub/other_sites/Lexicon/
+rsync -v lexicon.py lexicon_config.py pi@pi:/media/NAS/CLAHub/other_sites/Lexicon/
+ssh pi@pi "python3 /media/NAS/CLAHub/other_sites/Lexicon/lexicon.py"
+
