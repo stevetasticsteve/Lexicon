730aeadfbca61a9631b404a5b542aff5335be4d8
steve <stevetasticsteve@gmail.com>
2020-06-24 07:54:06 +1000

    Updated sync script

--- sync_script.sh
+++ sync_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scp lexicon.py pi@pi:/media/NAS/CLAHub/other_sites/Lexicon/
+scp kovol_verbs.py pi@pi:/media/NAS/CLAHub/other_sites/Lexicon/
 scp lexicon_config.py pi@pi:/media/NAS/CLAHub/other_sites/Lexicon/
 scp lexicon.css pi@pi:/media/NAS/CLAHub/other_sites/Lexicon/
 scp -r templates/ pi@pi:/media/NAS/CLAHub/other_sites/Lexicon
