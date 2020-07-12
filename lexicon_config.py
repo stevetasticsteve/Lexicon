20d933d84d86ac17850b10215ed6800e84ceeb49
steve <stevetasticsteve@gmail.com>
2020-07-12 20:22:29 +1000

    Created a local output folder to keep generated files seperate from code. Changed settings to output to there.

--- lexicon_config.py
+++ lexicon_config.py
@@ -1,7 +1,7 @@
 settings = {
     'language': 'Kovol',
-    'spreadsheet_name': '/media/NAS/Team Share/Kovol_lexicon.ods',
-    'target_folder': '/media/NAS/CLAHub/other_sites/Lexicon',  # the folder the web page should be created in
+    'spreadsheet_name': 'Kovol_lexicon.ods',
+    'target_folder': 'local_output',  # the folder the web page should be created in
     'sort': 'phonetics',  # order dictionary by 'phonetics' or 'orthography'
 }
 
