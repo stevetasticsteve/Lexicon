9f2365db1f2d76e8f1b57386731cdbb4e64b5fa0
steve <stevetasticsteve@gmail.com>
2021-02-06 21:03:06 +0000

    Trying to fix github CI test failures

--- tests/test_output.py
+++ tests/test_output.py
@@ -3,8 +3,9 @@ import os
 import unittest
 from unittest.mock import patch
 
+# from tests import fixtures
+import tests.fixtures
 from application_code import output
-from tests import fixtures
 
 
 class HTMLGenerationTests(unittest.TestCase):
@@ -25,25 +26,25 @@ class HTMLGenerationTests(unittest.TestCase):
             os.remove(os.path.join(self.test_folder, file))
 
     def test_generate_html(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_html(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_html(tests.fixtures.good_processed_data)
 
         files = (self.lex_page, self.reverse_page, self.check_page)  # help, errors
         for file in files:
             self.assertTrue(os.path.exists(file))
 
     def test_generate_lexicon_page_exists(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_lexicon_page(fixtures.good_processed_data, None)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_lexicon_page(tests.fixtures.good_processed_data, None)
         self.assertTrue(os.path.exists(self.lex_page))
 
     def test_generate_lexicon_page_contents(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_lexicon_page(fixtures.good_processed_data, None)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_lexicon_page(tests.fixtures.good_processed_data, None)
         with open(self.lex_page, 'r') as file:
             file = file.read()
             # check fixture data in the html
-            for word in fixtures.good_processed_data:
+            for word in tests.fixtures.good_processed_data:
                 self.assertIn(word['phon'], file)
                 self.assertIn(word['eng'], file)
                 self.assertIn(word['tpi'], file)
@@ -54,8 +55,8 @@ class HTMLGenerationTests(unittest.TestCase):
                 self.assertIn('<ul><a href="#{d}">{d}</a></ul>'.format(d=letter), file, 'letter beginnings missing')
 
     def test_generate_error_page_with_repeated_sense_errors(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_html(fixtures.repeated_sense_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_html(tests.fixtures.repeated_sense_processed_data)
         self.assertTrue(os.path.exists(self.error_page))
         with open(self.error_page, 'r') as file:
             file = file.read()
@@ -67,8 +68,8 @@ class HTMLGenerationTests(unittest.TestCase):
         self.assertIn('Errors in the data have been identified', file, 'Error message should be showing')
 
     def test_generate_error_page_with_pos_errors(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_html(fixtures.missing_pos_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_html(tests.fixtures.missing_pos_processed_data)
         self.assertTrue(os.path.exists(self.error_page))
         with open(self.error_page, 'r') as file:
             file = file.read()
@@ -80,8 +81,8 @@ class HTMLGenerationTests(unittest.TestCase):
         self.assertIn('Errors in the data have been identified', file, 'Error message should be showing')
 
     def test_error_page_multiple_errors(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_html(fixtures.multiple_error_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_html(tests.fixtures.multiple_error_processed_data)
         self.assertTrue(os.path.exists(self.error_page))
         with open(self.error_page, 'r') as file:
             file = file.read()
@@ -95,21 +96,21 @@ class HTMLGenerationTests(unittest.TestCase):
         self.assertIn('Errors in the data have been identified', file, 'Error message should be showing')
 
     def test_generate_error_page_without_errors(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_html(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_html(tests.fixtures.good_processed_data)
         self.assertFalse(os.path.exists(self.error_page))
         with open(self.lex_page, 'r') as file:
             file = file.read()
         self.assertNotIn('Errors in the data have been identified', file, 'Error message shouldn\'t be showing')
 
     def test_generate_Eng_page_exists(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_eng_page(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_eng_page(tests.fixtures.good_processed_data)
         self.assertTrue(os.path.exists(self.reverse_page))
 
     def test_generate_Eng_page_contents(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_eng_page(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_eng_page(tests.fixtures.good_processed_data)
 
         with open(self.reverse_page, 'r') as file:
             file = file.read()
@@ -117,13 +118,13 @@ class HTMLGenerationTests(unittest.TestCase):
             self.assertIn('<h3>rat</h3>', file, 'Entry missing')
 
     def test_generate_check_page_exists(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_check_page(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_check_page(tests.fixtures.good_processed_data)
         self.assertTrue(os.path.exists(self.check_page))
 
     def test_generate_check_page_contents(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.generate_check_page(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.generate_check_page(tests.fixtures.good_processed_data)
 
         with open(self.check_page, 'r') as file:
             file = file.read()
@@ -137,7 +138,7 @@ class HTMLGenerationTests(unittest.TestCase):
             self.assertIn('Template:', str(error.exception))
 
     def test_generate_context(self):
-        with patch('lexicon_config.settings', fixtures.settings):
+        with patch('lexicon_config.settings', tests.fixtures.settings):
             context = output.generate_context('title', 'header')
             self.assertEqual('Test', context['language'])
             self.assertEqual('title', context['title'])
@@ -149,7 +150,7 @@ class OtherFileGenerationTests(unittest.TestCase):
     def setUp(self):
         parent_folder = (os.path.abspath(os.path.join(__file__, '..')))
         self.paDb_path = os.path.join(parent_folder, 'test_output', '{language}_phonology_assistant.db'.format(
-            language=fixtures.settings['language']))
+            language=tests.fixtures.settings['language']))
         self.dataset_path = os.path.join(parent_folder, 'test_output', 'phonemic_dataset.csv')
 
     def tearDown(self):
@@ -159,17 +160,17 @@ class OtherFileGenerationTests(unittest.TestCase):
             os.remove(self.dataset_path)
 
     def test_create_phonemic_assistant_new_file_exists(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.create_phonemic_assistant_db(fixtures.good_processed_data, checked_only=False)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.create_phonemic_assistant_db(tests.fixtures.good_processed_data, checked_only=False)
         self.assertTrue(os.path.exists(self.paDb_path))
-        with patch('lexicon_config.settings', fixtures.settings):
+        with patch('lexicon_config.settings', tests.fixtures.settings):
             with self.assertRaises(AssertionError) as error:
-                output.create_phonemic_assistant_db(fixtures.good_processed_data, checked_only=True)
+                output.create_phonemic_assistant_db(tests.fixtures.good_processed_data, checked_only=True)
             self.assertIn('No checked data to work with!', str(error.exception))
 
     def test_create_phonemic_assistant_contents(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.create_phonemic_assistant_db(fixtures.good_processed_data, checked_only=False)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.create_phonemic_assistant_db(tests.fixtures.good_processed_data, checked_only=False)
 
         with open(self.paDb_path, 'r') as file:
             expected_contents = """\\_sh v3.0  400  PhoneticData
@@ -202,7 +203,7 @@ class OtherFileGenerationTests(unittest.TestCase):
             self.assertEqual(expected_contents, file.read())
 
     def test_create_dataset_csv(self):
-        with patch('lexicon_config.settings', fixtures.settings):
-            output.create_dataset_csv(fixtures.good_processed_data)
+        with patch('lexicon_config.settings', tests.fixtures.settings):
+            output.create_dataset_csv(tests.fixtures.good_processed_data)
 
         self.assertTrue(os.path.exists(self.dataset_path))
